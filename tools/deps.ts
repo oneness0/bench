86ae98410a513a5e325fa76e65ef50297e95085d
oneness <www.2000315@hotmail.com>
2024-12-30 02:52:34 +0000

    update deno

--- tools/deps.ts
+++ tools/deps.ts
@@ -2,6 +2,6 @@ export * from "https://deno.land/std@0.224.0/path/mod.ts";
 export { delay } from "https://deno.land/std@0.224.0/async/mod.ts";
 export { ensureDir } from "https://deno.land/std@0.224.0/fs/mod.ts";
 export { stringify } from "https://jsr.io/@std/yaml/1.0.5/mod.ts";
-export { prettyBytes } from "https://deno.land/std@0.224.0/fmt/bytes.ts";
+export { format as prettyBytes } from "https://deno.land/std@0.224.0/fmt/bytes.ts";
 export { markdownTable } from "https://esm.sh/markdown-table@3.0.2";
 export { default as prettyMilliseconds } from "https://esm.sh/pretty-ms@8.0.0";
